bb1c02b31892fa4280a90805e1f2ee99ffdae866
phamducdat <phamducdat2402@gmail.com>
2023-05-27 19:45:59 +0700

    fix: allow CrossOrigin("*")

--- src/main/java/net/javaguides/springboot/springboot/SpringbootDockerDemoApplication.java
+++ src/main/java/net/javaguides/springboot/springboot/SpringbootDockerDemoApplication.java
@@ -2,11 +2,13 @@ package net.javaguides.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin("*")
 public class SpringbootDockerDemoApplication {
 
 
